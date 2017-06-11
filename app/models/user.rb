88047331ec8f0e218c7390073c27bed6001dbb83
Pablo Crivella <pablocrivella@gmail.com>
2017-06-11 18:31:23 +0200

    Implement user sign up adding name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :fraternities, through: :memberships
   has_many :organizer_assignations, foreign_key: :organizer_id
   has_many :meetings, through: :organizer_assignations
+
+  validates :name, presence: true
 end
