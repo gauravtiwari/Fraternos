4f57241e8ff2784a6cf195acc96d12acea010c31
Pablo Crivella <pablocrivella@gmail.com>
2017-09-24 23:13:23 +0200

    Remove debt and payment policies

--- app/policies/debt_policy.rb
+++
@@ -1,9 +0,0 @@
-class DebtPolicy < ApplicationPolicy
-  def new?
-    create?
-  end
-
-  def create?
-    fraternity_admin?
-  end
-end
--- app/policies/payment_policy.rb
+++
@@ -1,9 +0,0 @@
-class PaymentPolicy < ApplicationPolicy
-  def new?
-    create?
-  end
-
-  def create?
-    fraternity_admin?
-  end
-end
