24183bdc2b16e72370bc31d9748112aa47e40ce3
Roberto Pando <roberto_pando@hotmail.com>
2017-11-04 20:00:30 +0100

    update translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,9 +63,9 @@ module ApplicationHelper
 
   def change_locale_path(options = {})
     if I18n.locale == I18n.default_locale
-      link_to('Español', { locale: :es }, **options)
+      link_to(t('general.language'), { locale: :es }, **options)
     else
-      link_to('English', { locale: :en }, **options)
+      link_to(t('general.language'), { locale: :en }, **options)
     end
   end
 end
